e4e8f4e7a0bfc5014d9f6900d000e30cef574c5b
paras sharma <sharma.paras4444@gmail.com>
2024-11-29 15:19:32 +0530

    updated to code crpyto issue on server

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { EventType, PublicClientApplication } from '@azure/msal-browser';
-import { msalConfig } from './auth-config';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { EventType, PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "./auth-config";
 
-const msalInstance = new PublicClientApplication(msalConfig);
+let msalInstance;
 
+if (typeof window !== "undefined") {
+  msalInstance = new PublicClientApplication(msalConfig);
 
-if(!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-  msalInstance.setActiveAccount(msalInstance.getActiveAccount()[0]);
+  if (
+    !msalInstance.getActiveAccount() &&
+    msalInstance.getAllAccounts().length > 0
+  ) {
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+  }
+
+  msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+      const account = event.payload.account;
+      msalInstance.setActiveAccount(account);
+    }
+  });
 }
 
-
-msalInstance.addEventCallback((event)=>{
-  if(event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
-    const account = event.payload.account;
-    msalInstance.setActiveAccount(account);
-  }
-})
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App instance = {msalInstance}/>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App instance={msalInstance} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
