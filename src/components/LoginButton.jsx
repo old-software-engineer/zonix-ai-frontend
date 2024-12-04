322a3de56094839d7543b626c6b3016ac84eda50
paras sharma <sharma.paras4444@gmail.com>
2024-12-04 22:47:06 +0530

    Updated loginbutton

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoginButton = () => {
   const handleLogin = () => {
-    window.location.href = "http://localhost:8000/auth/login";
+    window.location.href = `${process.env.REACT_APP_API_URL}/auth/login`;
   };
 
   return <button className="px-10 py-3 text-white" onClick={handleLogin}>Log in with Microsoft</button>;
